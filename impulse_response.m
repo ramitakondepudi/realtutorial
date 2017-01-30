668da32093ff760e008dee0d47a8234d0e75926e
Ramita Kondepudi <rkondepudi@hmc.edu>
2017-01-29 22:25:31 -0800

    Impulse response fixed and plot
    :x!

--- impulse_response.m
+++ impulse_response.m
@@ -8,8 +8,11 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+%fixed H by Ramita Kondepudi 
+f=0:10; 
+plot(f,H);
 
 end
 
